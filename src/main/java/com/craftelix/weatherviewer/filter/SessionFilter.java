667915c51da5f1642de70ff6402bbbb8c1f04140
leofinder <imrhyde@yandex.ru>
2024-10-23 22:30:10 +0500

    feat: add SessionFilter

---
+++ src/main/java/com/craftelix/weatherviewer/filter/SessionFilter.java
@@ -0,0 +1,89 @@
+package com.craftelix.weatherviewer.filter;
+
+import com.craftelix.weatherviewer.entity.Session;
+import com.craftelix.weatherviewer.service.SessionService;
+import jakarta.servlet.*;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class SessionFilter extends OncePerRequestFilter {
+
+    private static final String SESSION_ID = "sessionId";
+
+    private final SessionService sessionService;
+
+    @Autowired
+    public SessionFilter(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Optional<Cookie> sessionCookie = getSessionCookie(request);
+
+        if (sessionCookie.isEmpty()) {
+            response.sendRedirect("/login");
+            return;
+        }
+
+        UUID sessionId = UUID.fromString(sessionCookie.get().getValue());
+
+        if (isSessionValid(sessionId)) {
+            filterChain.doFilter(request, response);
+        } else {
+            sessionService.removeSession(sessionId);
+            response.sendRedirect("/login");
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        boolean shouldFilter = true;
+        String path = request.getRequestURI();
+        if ("/login".equals(path) || "/logout".equals(path) || "/register".equals(path)) {
+            shouldFilter = false;
+        }
+        return !shouldFilter;
+    }
+
+    private Optional<Cookie> getSessionCookie(HttpServletRequest httpRequest) {
+        Optional<Cookie> sessionCookie = Optional.empty();
+
+        Cookie[] cookies = httpRequest.getCookies();
+
+        if (cookies != null) {
+            sessionCookie = Arrays.stream(cookies)
+                    .filter(cookie -> SESSION_ID.equals(cookie.getName()))
+                    .findFirst();
+        }
+
+        return sessionCookie;
+    }
+
+    private boolean isSessionValid(UUID sessionId) {
+        boolean isValid = false;
+
+        Session session = sessionService.findSession(sessionId);
+        LocalDateTime expiresAt = session.getExpiresAt();
+        if (expiresAt.isAfter(LocalDateTime.now())) {
+            isValid = true;
+        }
+
+        return isValid;
+    }
+
+
+}
