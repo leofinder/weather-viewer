2c587edeee428c9c4e96ecd426ba5f271d984412
leofinder <imrhyde@yandex.ru>
2024-10-27 18:46:41 +0500

    feat: add LocationMapper

---
+++ src/main/java/com/craftelix/weatherviewer/mapper/LocationMapper.java
@@ -0,0 +1,25 @@
+package com.craftelix.weatherviewer.mapper;
+
+import com.craftelix.weatherviewer.dto.LocationRequestDto;
+import com.craftelix.weatherviewer.dto.openweather.LocationDto;
+import com.craftelix.weatherviewer.entity.Location;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface LocationMapper {
+    LocationMapper INSTANCE = Mappers.getMapper(LocationMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    Location toEntity(LocationRequestDto locationRequestDto);
+
+    @Mapping(target = "id", ignore = true)
+    Location toEntity(LocationDto locationDto);
+
+    LocationDto toDto(Location location);
+
+    List<LocationDto> toDto(List<Location> locations);
+}
