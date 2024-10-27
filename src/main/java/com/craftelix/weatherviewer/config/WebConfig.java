0d6c90e8ea13e60b6a69ba8c66b2d544517ea735
leofinder <imrhyde@yandex.ru>
2024-10-27 18:54:38 +0500

    feat: configure static resource handling

--- src/main/java/com/craftelix/weatherviewer/config/WebConfig.java
+++ src/main/java/com/craftelix/weatherviewer/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -45,4 +46,10 @@ public class WebConfig implements WebMvcConfigurer {
         return viewResolver;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 }
