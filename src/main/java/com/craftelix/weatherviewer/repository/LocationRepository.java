862fc72c7dac4d2476c2924707f70470012b4002
leofinder <imrhyde@yandex.ru>
2024-10-27 18:58:57 +0500

    feat: add location repository find methods

--- src/main/java/com/craftelix/weatherviewer/repository/LocationRepository.java
+++ src/main/java/com/craftelix/weatherviewer/repository/LocationRepository.java
@@ -1,9 +1,28 @@
 package com.craftelix.weatherviewer.repository;
 
 import com.craftelix.weatherviewer.entity.Location;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LocationRepository extends CrudRepository<Location, Long> {
+
+    @Query(""" 
+            SELECT *
+            FROM locations
+            WHERE name = :#{#location.name}
+                AND latitude = :#{#location.latitude}
+                AND longitude = :#{#location.longitude}
+                AND user_id = :#{#location.userId}
+            LIMIT 1
+            """)
+    Optional<Location> findByLocationInfo(@Param("location") Location location);
+
+    List<Location> findByUserId(Long userId);
+
 }
