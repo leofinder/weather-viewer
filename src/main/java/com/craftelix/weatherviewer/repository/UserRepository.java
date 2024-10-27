c44eb0ea7388a5c3753feaa2f137c5a7e044c0fe
leofinder <imrhyde@yandex.ru>
2024-10-27 18:47:21 +0500

    feat: add query findBySessionId

--- src/main/java/com/craftelix/weatherviewer/repository/UserRepository.java
+++ src/main/java/com/craftelix/weatherviewer/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.craftelix.weatherviewer.repository;
 
 import com.craftelix.weatherviewer.entity.User;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
@@ -13,4 +16,11 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByLogin(String login);
 
+    @Query("""
+            SELECT *
+            FROM users
+            JOIN sessions ON users.id = sessions.user_id
+            WHERE sessions.id = :sessionId
+            """)
+    Optional<User> findBySessionId(@Param("sessionId") UUID sessionId);
 }
