a24e7712ea03e6cb323fe4ad16f15d869935bef7
leofinder <imrhyde@yandex.ru>
2024-11-01 16:15:33 +0500

    feat: implement error pages

--- src/main/java/com/craftelix/weatherviewer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/craftelix/weatherviewer/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.craftelix.weatherviewer.exception;
 
-import com.craftelix.weatherviewer.dto.UserSignupDto;
 import com.craftelix.weatherviewer.dto.UserLoginDto;
+import com.craftelix.weatherviewer.dto.UserSignupDto;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.List;
 import java.util.Objects;
@@ -47,6 +50,20 @@ public class GlobalExceptionHandler {
         ModelAndView modelAndView = new ModelAndView("/login");
         modelAndView.addObject("user", new UserLoginDto());
         return modelAndView;
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundExceptions(final Exception ex) {
+        log.error("HTTP Response Code: {}. {}", HttpStatus.NOT_FOUND, ex.getMessage(), ex);
+        return "/errors/404";
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleRuntimeExceptions(final Exception ex) {
+        log.error("HTTP Response Code: {}. {}", HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        return "/errors/500";
     }
 
     private String buildErrorMessage(BindingResult bindingResult, String... fieldsToCheck) {
