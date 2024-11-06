8f674ad14d5e31c4686bab65bc027639a2947985
leofinder <imrhyde@yandex.ru>
2024-11-06 16:48:15 +0500

    style: replace lombok wildcard import with explicit imports

--- src/main/java/com/craftelix/weatherviewer/entity/Location.java
+++ src/main/java/com/craftelix/weatherviewer/entity/Location.java
@@ -1,6 +1,10 @@
 package com.craftelix.weatherviewer.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
