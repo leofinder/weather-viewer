221c042e1d89c4ca682b4ebd6f68ba00b489bef2
leofinder <imrhyde@yandex.ru>
2024-11-08 13:34:18 +0500

    refactor: introduce WeatherService interface with multiple implementations

---
+++ src/main/java/com/craftelix/weatherviewer/service/OpenWeatherApiService.java
@@ -0,0 +1,49 @@
+package com.craftelix.weatherviewer.service;
+
+import com.craftelix.weatherviewer.config.RestClientConfig;
+import com.craftelix.weatherviewer.dto.api.LocationApiDto;
+import com.craftelix.weatherviewer.dto.api.WeatherApiDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OpenWeatherApiService implements WeatherService {
+
+    private final RestClient restClient;
+
+    private final RestClientConfig restClientConfig;
+
+    public List<LocationApiDto> findLocationsByName(String city) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(restClientConfig.getGeoApiPath())
+                        .queryParam("q", city)
+                        .queryParam("limit", restClientConfig.getLimit())
+                        .queryParam("appid", restClientConfig.getApiKey())
+                        .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() { });
+    }
+
+    public WeatherApiDto getWeatherData(BigDecimal latitude, BigDecimal longitude) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(restClientConfig.getDataApiPath())
+                        .queryParam("lat", latitude)
+                        .queryParam("lon", longitude)
+                        .queryParam("appid", restClientConfig.getApiKey())
+                        .queryParam("units", restClientConfig.getUnits())
+                        .build())
+                .retrieve()
+                .body(WeatherApiDto.class);
+    }
+
+}
--- src/main/java/com/craftelix/weatherviewer/service/WeatherService.java
+++ src/main/java/com/craftelix/weatherviewer/service/WeatherService.java
@@ -1,54 +1,14 @@
 package com.craftelix.weatherviewer.service;
 
-import com.craftelix.weatherviewer.config.RestClientConfig;
 import com.craftelix.weatherviewer.dto.api.LocationApiDto;
 import com.craftelix.weatherviewer.dto.api.WeatherApiDto;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Slf4j
-@Service
-public class WeatherService {
+public interface WeatherService {
 
-    private final RestClient restClient;
+    List<LocationApiDto> findLocationsByName(String city);
 
-    private final RestClientConfig restClientConfig;
-
-    @Autowired
-    public WeatherService(RestClient restClient, RestClientConfig restClientConfig) {
-        this.restClient = restClient;
-        this.restClientConfig = restClientConfig;
-    }
-
-    public List<LocationApiDto> findLocationsByName(String city) {
-        return restClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(restClientConfig.getGeoApiPath())
-                        .queryParam("q", city)
-                        .queryParam("limit", restClientConfig.getLimit())
-                        .queryParam("appid", restClientConfig.getApiKey())
-                        .build())
-                .retrieve()
-                .body(new ParameterizedTypeReference<>() { });
-    }
-
-    public WeatherApiDto getWeatherData(BigDecimal latitude, BigDecimal longitude) {
-        return restClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(restClientConfig.getDataApiPath())
-                        .queryParam("lat", latitude)
-                        .queryParam("lon", longitude)
-                        .queryParam("appid", restClientConfig.getApiKey())
-                        .queryParam("units", restClientConfig.getUnits())
-                        .build())
-                .retrieve()
-                .body(WeatherApiDto.class);
-    }
-
+    WeatherApiDto getWeatherData(BigDecimal latitude, BigDecimal longitude);
 }
