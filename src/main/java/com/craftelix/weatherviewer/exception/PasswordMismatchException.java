65911bf271586891e2e4e1769f4dc2895d285f2e
leofinder <imrhyde@yandex.ru>
2024-11-13 18:02:19 +0500

    refactor: remove unused exception

--- src/main/java/com/craftelix/weatherviewer/exception/PasswordMismatchException.java
+++
@@ -1,8 +0,0 @@
-package com.craftelix.weatherviewer.exception;
-
-public class PasswordMismatchException extends RuntimeException {
-
-    public PasswordMismatchException(String message) {
-        super(message);
-    }
-}
